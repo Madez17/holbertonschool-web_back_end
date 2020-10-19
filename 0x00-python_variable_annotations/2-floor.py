b562857553a36742e48fda4c7688a916ed688519
Madez17 <mafe.mendez.m@gmail.com>
2020-10-19 17:41:59 -0500

    type-annotated function- Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+"""  Basic annotations -  floor   """
+
+
+def floor(n: float) -> int:
+    """ function concat that takes a string str1 and a string str2 """
+    return int(n)
