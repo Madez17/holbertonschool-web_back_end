a2a305c8339e81c8bb307b8b1ce468bc0560787c
Madez17 <mafe.mendez.m@gmail.com>
2020-10-19 16:35:25 -0500

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+"""  Basic annotations - concat  """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function concat that takes a string str1 and a string str2 """
+    return str1 + str2
