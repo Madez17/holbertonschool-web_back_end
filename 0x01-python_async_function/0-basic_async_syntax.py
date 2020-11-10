b4b87d5dd4d6ae469b48d053ae648f864b14d36d
Madez <mafe.mendez.m@gmail.com>
2020-11-10 14:44:50 -0500

    The basics of async - [corutine]

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,15 @@
 import random
 
 
+""" Async """
+
+
 def wait_random(max_delay=10):
+
+    """
+    synchronous coroutine that takes
+    in an integer argument
+
+    """
     yield
     return random.uniform(0, max_delay)
